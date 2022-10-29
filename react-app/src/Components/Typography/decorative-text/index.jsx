3be94b4032553e46de06edb57fb6cf55f38012cd
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-10-29 16:00:41 +0200

    Fix decorative text dynamic font size

--- react-app/src/Components/Typography/decorative-text/index.jsx
+++ react-app/src/Components/Typography/decorative-text/index.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import useWindowSize from "../../../Hooks/use-window-size";
 
 function DecorativeText({ children, size = 80 }) {
   const windowSize = useWindowSize();
+  const [fontSize, setFontSize] = useState(size);
+
+  useEffect(() => {
+    if (windowSize.innerWidth <= 768 && windowSize.innerWidth > 425) {
+      setFontSize(80);
+    } else if (windowSize.innerWidth <= 425) {
+      setFontSize(64);
+    } else if (windowSize.innerWidth > 768) {
+      setFontSize(size);
+    }
+  }, [windowSize]);
 
   return (
-    <span
+    <div
       className="decorative-text"
-      style={{ fontSize: windowSize.innerWidth >= 425 ? `${size}px` : `80px` }}
+      style={{
+        fontSize: `${fontSize}px`,
+      }}
     >
       {children}
-    </span>
+    </div>
   );
 }
 
