18cfcc4c0bfb696d9e851a8ae8c695a053208ff5
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-10-29 20:23:53 +0200

    install axios. add custom axios client

---
+++ react-app/src/Functions/create-axios/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+/**
+ *
+ * @param {object} auth Optional object provided using AuthContext
+ * @returns Axios client with authorization headers, if any.
+ */
+export default function createAxios(auth) {
+  const client = axios.create();
+  if (auth) {
+    client.defaults.headers.common["Authorization"] = auth.token;
+  }
+
+  return client;
+}
