8a077d5faf7d1881e7821478a958ee393fef638c
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-11-09 17:47:30 +0100

    add app sidebar link to own user page

--- react-app/src/Components/navbar/app-sidebar/sidebar-user/index.jsx
+++ react-app/src/Components/navbar/app-sidebar/sidebar-user/index.jsx
@@ -1,7 +1,16 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../../../Context/auth-context";
 
 function SidebarUser() {
-  return <div className="align-self-start">SidebarUser</div>;
+  const [auth, setAuth] = useContext(AuthContext);
+  return (
+    <div className="align-self-start">
+      {auth && <Link to={`/user/${auth.name}`}>{auth.name}</Link>}
+    </div>
+  );
 }
 
 export default SidebarUser;
