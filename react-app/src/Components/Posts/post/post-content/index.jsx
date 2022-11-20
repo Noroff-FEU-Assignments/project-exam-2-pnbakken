3f5b4a41acb5aac07867849002d5b1f967647daa
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-11-20 22:57:33 +0100

    better format post date string

--- react-app/src/Components/Posts/post/post-content/index.jsx
+++ react-app/src/Components/Posts/post/post-content/index.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import ProfileImage from "../../../User/profile-image";
 
 function PostContent({ data }) {
+  const dateCreated = new Date(data.dateCreated);
+
   return (
-    <div className="post-content p-2 flex-column gap-sm">
+    <div className="post-content p-3 flex-column gap-sm">
       <div className="post-header flex-row wrap full-width align-center gap-xs">
         <Link to={`user/${data.author.name}`}>
           <ProfileImage src={data.author.avatar ? data.author.avatar : ""} />
@@ -14,6 +16,11 @@ function PostContent({ data }) {
           <Link to={`/user/${data.author.name}`} className="post-author-name">
             <span className="author-name">{data.author.name}</span>
           </Link>
+        </div>
+      </div>
+      <div className="post-main flex-column align-self-center full-width gap-xs">
+        <div>
+          <div className="post-title align-self-start">{data.title}</div>
           <div className="flex-row gap-xxs">
             <span className="post-date">
               {new Date(data.created).toLocaleTimeString()}
@@ -23,9 +30,6 @@ function PostContent({ data }) {
             </span>
           </div>
         </div>
-      </div>
-      <div className="post-main flex-column align-self-center full-width gap-xs">
-        <div className="post-title align-self-start">{data.title}</div>
         <div className="post-body flex-column align-center full-width gap-md">
           <p>{data.body}</p>
           {data.media && (
