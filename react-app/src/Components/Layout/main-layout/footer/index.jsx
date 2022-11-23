bfb3bdb6f23c74593a7fdbfb02ae9f8067e78e7b
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-11-23 16:49:49 +0100

    add app version envrionment variable

--- react-app/src/Components/Layout/main-layout/footer/index.jsx
+++ react-app/src/Components/Layout/main-layout/footer/index.jsx
@@ -4,7 +4,11 @@ import DeleteUser from "../../../Buttons/delete-user";
 import "./index.style.scss";
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      Just Post - version <code>{process.env.REACT_APP_VERSION}</code>
+    </footer>
+  );
 }
 
 export default Footer;
