02e4c90a6367117f9338430face0cac4c0930e61
pnbakken <74681160+pnbakken@users.noreply.github.com>
2022-12-02 20:11:51 +0100

    Trying to improve single posst display method. Not working as intended  but nothing seems broken either so keeping the changes

--- src/Pages/Page-Routes/index.jsx
+++ src/Pages/Page-Routes/index.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import ScrollToTop from "../../Components/Utility-Components/scroll-to-top";
 import AllUsers from "../All-Users";
 import FrontPage from "../front-page";
 import Home from "../home";
 import Login from "../login";
 import Register from "../register";
+import SinglePost from "../Post";
 import UserPage from "../User-Page";
 import UserSettings from "../user-settings";
 
 function PageRoutes() {
   return (
-    <ScrollToTop>
-      <Routes>
-        <Route path="/" element={<FrontPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/user/:name" element={<UserPage />} />
-        <Route path="/user/:name/settings" element={<UserSettings />} />
-        <Route path="/users" element={<AllUsers />} />
-        <Route path="/users:limit" element={<AllUsers />} />
-      </Routes>
-    </ScrollToTop>
+    <Routes>
+      <Route path="/" element={<FrontPage />} />
+      <Route path="/home" element={<Home />} />
+      {/* {<Route path="/single-post/:id" element={<SinglePost />} />} */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/user/:name" element={<UserPage />} />
+
+      <Route path="/user/:name/settings" element={<UserSettings />} />
+      <Route path="/users" element={<AllUsers />} />
+      <Route path="/users:limit" element={<AllUsers />} />
+    </Routes>
   );
 }
 
